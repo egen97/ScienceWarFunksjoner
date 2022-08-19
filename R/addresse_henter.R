3bddb3908d5fbcdc5e2a01da0c872788705c7f61
Eric G. E. Nilsen <e.g.e.nilsen@stv.uio.no>
2022-08-19 15:59:20 +0200

    Lagt til dokumentasjon av funksjonene

---
+++ R/addresse_henter.R
@@ -0,0 +1,23 @@
+#' Title
+#'
+#' @param x Datasett, enten pull_x dataene eller All_Data
+#' @param city_column kollone som inneholder bynavn
+#' @param country_column kollone som inneholder landnavn
+#'
+#' @return En tibble med to kolloner, City og Country
+#' @export
+#'
+#' @examples
+#' adresse_henter(Pull_2)
+adresse_henter <- function(x, city_column = "address.city", country_column = "address.country") {
+  city_column_q <- dplyr::enquo(city_column)
+  country_column_q <- dplyr::enquo(country_column)
+
+  x <- x %>%
+    dplyr::select(!!city_column_q, !!country_column_q) %>%
+    dplyr::rename(
+      "City" = city_column,
+      "Country" = country_column
+    )
+  return(x)
+}
--- R/gruppe_summerer.R
+++ R/gruppe_summerer.R
@@ -1,3 +1,14 @@
+#' gruppe_summerer
+#'
+#' @param name gir navn til ny kollone med summen av fagene innenfor denne
+#' @param fagGruppe hvilket institutt som skal hentes
+#' @param ... kan velge om det ønskes publikasjoner eller siteringer, returneres til kattegori_henter funksjonen
+#' @import data.table
+#' @return En data.table hvor fagene er redusert ned til kattegoriene bestemt i fagkattegorier filen
+#' @export
+#'
+#' @examples
+#' gruppe_summerer(All_Data)
 gruppe_summerer <- function(name, fagGruppe, ...){
   datanew <- data.table::copy(FagData)
   datanew <- datanew[, .(sum(.SD)), by = .(id, year), .SDcols = Kattegori_Henter(fagGruppe,...)]
