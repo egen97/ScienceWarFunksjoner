41e98418f2a02bc4ae3155e7513d3ac47c66e3e6
Eric G. E. Nilsen <e.g.e.nilsen@stv.uio.no>
2022-08-22 12:40:53 +0200

    Funker det nå? :(

--- R/gruppe_summerer.R
+++ R/gruppe_summerer.R
@@ -3,6 +3,8 @@
 #' @param name gir navn til ny kollone med summen av fagene innenfor denne
 #' @param fagGruppe hvilket institutt som skal hentes
 #' @param ... kan velge om det ønskes publikasjoner eller siteringer, returneres til kattegori_henter funksjonen
+#' @param x datasettet som skal kollapses
+#' @import dplyr
 #' @import data.table
 #' @return En data.table hvor fagene er redusert ned til kattegoriene bestemt i fagkattegorier filen
 #' @export
@@ -11,8 +13,8 @@
 #' \dontrun{
 #' gruppe_summerer(All_Data)
 #' }
-gruppe_summerer <- function(name, fagGruppe, ...){
-  datanew <- data.table::copy(FagData)
+gruppe_summerer <- function(x, name, fagGruppe, ...){
+  datanew <- data.table::copy(x)
   datanew <- datanew[, .(sum(.SD)), by = .(id, year), .SDcols = Kattegori_Henter(fagGruppe,...)]
   rename(datanew, !!quo_name(name) := "V1")
 }
